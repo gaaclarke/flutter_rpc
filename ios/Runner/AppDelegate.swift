b296bc665c9b32af972aa0f9d01bcff3eedbfd9a
Aaron Clarke <aaclarke@google.com>
2019-12-13 14:50:16 -0800

    Gave sayGoodbye its own method

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,8 +8,22 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    DispatchQueue.main.asyncAfter(deadline: .now() + 1.0, execute: doMessaging)
+    setupFlutterRpc()
+    DispatchQueue.main.asyncAfter(deadline: .now() + 1.0, execute: sendMessage)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func setupFlutterRpc() {
+    let vc = self.window.rootViewController as! FlutterViewController
+    let binaryMessenger = vc.engine!.binaryMessenger
+    setupSayGoodbye(binaryMessenger: binaryMessenger, function: sayGoodbye)
+  }
+
+  func sayGoodbye(request: HelloRequest) -> HelloReply {
+    print("Goodbye from " + request.name)
+    var reply = HelloReply()
+    reply.message = "Message received from swift!"
+    return reply
   }
 
   // This could be auto-generated.
@@ -61,18 +75,11 @@ import Flutter
     }
   }
 
-  func doMessaging() {
+  func sendMessage() {
+    print("Send message")
     let vc = self.window.rootViewController as! FlutterViewController
     let binaryMessenger = vc.engine!.binaryMessenger
 
-    setupSayGoodbye(binaryMessenger: binaryMessenger) { (request) -> HelloReply in
-      print("Goodbye from " + request.name)
-      var reply = HelloReply()
-      reply.message = "Message received from swift!"
-      return reply
-    }
-
-    print("Send message")
     var request = HelloRequest()
     request.name = "IosHost"
     sayHello(binaryMessenger: binaryMessenger, request: request) { (helloReply) in
